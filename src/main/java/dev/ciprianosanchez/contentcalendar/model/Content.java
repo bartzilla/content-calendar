6adaae611d84f8b823c228b953680753e201f3b7
Cipriano Sanchez <ciprianosanchez@gmail.com>
2023-08-21 17:38:21 +0200

    Added content endpoint

---
+++ src/main/java/dev/ciprianosanchez/contentcalendar/model/Content.java
@@ -0,0 +1,17 @@
+package dev.ciprianosanchez.contentcalendar.model;
+
+import java.time.LocalDateTime;
+
+public record Content(
+        Integer id,
+        String title,
+        String desc,
+        Status status,
+        Type type,
+        LocalDateTime dateCreated,
+        LocalDateTime dateUpdated,
+        String url
+) {
+
+
+}
---
+++ src/main/java/dev/ciprianosanchez/contentcalendar/model/Status.java
@@ -0,0 +1,8 @@
+package dev.ciprianosanchez.contentcalendar.model;
+
+public enum Status {
+    IDEA,
+    IN_PROGRESS,
+    COMPLETED,
+    PUBLISH
+}
