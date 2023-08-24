775386416ce7b5b1eb55c444f21b8a9e4a43926e
Cipriano Sanchez <ciprianosanchez@gmail.com>
2023-08-24 16:24:00 +0200

    Added PostConstruct Content

--- src/main/java/dev/ciprianosanchez/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/ciprianosanchez/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package dev.ciprianosanchez.contentcalendar.repository;
 
 import dev.ciprianosanchez.contentcalendar.model.Content;
+import dev.ciprianosanchez.contentcalendar.model.Status;
+import dev.ciprianosanchez.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -21,4 +25,20 @@ public class ContentCollectionRepository {
     public Optional<Content> findById(Integer id) {
         return content.stream().filter(c -> c.id().equals(id)).findFirst();
     }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(1,
+                "My first blog post",
+                "My fist blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                ""
+        );
+        content.add(c);
+    }
+
+
 }
