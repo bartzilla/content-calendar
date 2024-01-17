3457ec2db635489d015dc9f1aefefaffa1bb22be
Cipriano Sanchez <ciprianosanchez@gmail.com>
2024-01-17 14:58:02 +0100

    Testing GitHub Copilot

--- src/main/java/dev/ciprianosanchez/contentcalendar/Application.java
+++ src/main/java/dev/ciprianosanchez/contentcalendar/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    // Run the application by running the main method
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
